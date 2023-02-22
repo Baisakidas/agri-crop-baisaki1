56e1f07ed8815ac95ea71759cef245288f574080
Baisakidas <121195103+Baisakidas@users.noreply.github.com>
2023-02-22 23:34:32 +0530

    Add files via upload

---
+++ precache-manifest.18e782fa593992af4d93ca1bcf43ff67.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b99130708f24716c68db8cd5e43686",
+    "url": "/index.html"
+  },
+  {
+    "revision": "947184765909a140961e",
+    "url": "/static/css/main.6ee0aac9.chunk.css"
+  },
+  {
+    "revision": "a4d014c55a5d1b619a3a",
+    "url": "/static/js/2.839e7d7c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.839e7d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947184765909a140961e",
+    "url": "/static/js/main.59cf30e4.chunk.js"
+  },
+  {
+    "revision": "a5ad72c7b035ab9759f9",
+    "url": "/static/js/runtime-main.9dfdc2fd.js"
+  },
+  {
+    "revision": "8698e162cb63034727c28e456466452c",
+    "url": "/static/media/cropData.8698e162.csv"
+  },
+  {
+    "revision": "7f38a80e3e495d741cd578b187a44b73",
+    "url": "/static/media/loading_data.7f38a80e.gif"
+  },
+  {
+    "revision": "9b5ff49417588082d68c68314b9de4d7",
+    "url": "/static/media/logo.9b5ff494.png"
+  }
+]);
